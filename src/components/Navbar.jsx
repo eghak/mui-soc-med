bfa130c37bcaab16c628b35838bf0eaf32f7c57a
Egha Kusuma <eghakusuma1@gmail.com>
2022-05-13 23:31:57 -0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,110 @@
-import React from "react";
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  styled,
+  Toolbar,
+  Typography,
+  Box,
+  InputBase,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import EmojiPeopleTwoToneIcon from "@mui/icons-material/EmojiPeopleTwoTone";
-// import { Search } from "@mui/icons-material";
+import { Notifications, Mail } from "@mui/icons-material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
-  backgroundColor: "darkred",
+  backgroundColor: "purple",
 });
 
-const Search = styled("div") (({theme}) => ({
+const Search = styled("div")(({ theme }) => ({
   backgroundColor: "white",
-}))
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: 20,
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: 20,
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <AppBar position="sticky">
       <StyledToolbar>
-        <Typography variant="h6" sx={{display: {xs:"none", sm:"block"}}} >SOCIAL MEDIA</Typography>
-        <EmojiPeopleTwoToneIcon sx={{display: {xs:"block", sm:"none"}}} />
-        <Search>Search</Search>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          SOCIAL MEDIA
+        </Typography>
+
+        <EmojiPeopleTwoToneIcon sx={{ display: { xs: "block", sm: "none" } }} />
+
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://randomuser.me/api/portraits/men/4.jpg"
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://randomuser.me/api/portraits/men/4.jpg"
+            onClick={(e) => setOpen(true)}
+          />
+          <Typography variant="span">Mike</Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        // anchorEl={anchorEl}
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
